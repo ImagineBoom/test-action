25b67b859bd2f49d5bdb86bb29f842e216dcac21
ImagineBoom <imagineboom@foxmail.com>
2023-10-30 22:05:18 +0800

    feat: inno setup ok. msix mid

---
+++ run.sh
@@ -0,0 +1,10 @@
+C:\"Program Files (x86)"\"Inno Setup 6"\ISCC.exe `
+/DMyAppName="test_action" `
+/DMyAppExeName="test_action.exe" `
+/DMyAppVersion="2.0.0" `
+/DDepDir="D:\Workspace\test_action\build\windows\runner\Release" `
+/Qp `
+/O"dist" `
+/F"test_action" `
+/Sbyparam=$p `
+"D:\Workspace\test_action\packaging\exe\InnoSteupScript.iss"
